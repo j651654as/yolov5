81cd786fc923b586666e9c696742f5929bddb9ab
j651654as <59286593+j651654as@users.noreply.github.com>
2022-02-14 18:20:25 +0330

    Update and rename train.py to trainNew.py

--- trainNew.py
+++ trainNew.py
@@ -32,6 +32,9 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.optim import SGD, Adam, AdamW, lr_scheduler
 from tqdm import tqdm
 
+import warnings
+warnings.warn('User provided device_type of \'cuda\', but CUDA is not available. Disabling')
+
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path:
